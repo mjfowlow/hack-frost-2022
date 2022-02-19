04b6e0a3feeeb9c52809f8b45cd926819261f6ec
Mark Fowlow <markfowlow@gmail.com>
2022-02-19 14:24:06 -0330

    Made changes to WarningNotification

--- src/components/WarningNotification.jsx
+++ src/components/WarningNotification.jsx
@@ -1,9 +1,20 @@
 import { Box, Text } from "@chakra-ui/react";
+import Banner from 'react-js-banner';
+import { useState } from 'react';
 
 const WarningNotification = () => {
-  return (
+  const [cssState] = useState({backgroundColor: "#F94D1C", color: "#FFF", borderRadius: "25px", padding: "25px", margin:"25px"});
+  
+  return (  
     <Box>
-      <Text>Warning Notification</Text>
+      <Banner 
+	      title = "Warning!"
+        css={cssState} 
+      />
+
+      <div style={{border: '2px solid black',  margin: "50px", borderRadius: "25px", justifyContent: "center"}}>
+        <Text>Test</Text>
+      </div>
     </Box>
   );
 };
